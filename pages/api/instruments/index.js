77b6a5dcc45b0ad72a3d4402f39c0f63cae37d2c
Carla Daniela Rendon <carla.rendon@talosdigital.com>
2022-08-28 19:53:36 -0500

    changes

--- pages/api/instruments/index.js
+++ pages/api/instruments/index.js
@@ -4,11 +4,13 @@ export default async function (req, res) {
   const prisma = new PrismaClient();
   if (req.method === "GET") {
     const instruments = await prisma.instrument.findMany();
+    console.log(instruments)
     prisma.$disconnect();
     return res.send(instruments);
   } else if (req.method === "POST") {
     const { body: data } = req;
     const newInstrument = await prisma.instrument.create({ data });
+    console.log(newInstrument)
     prisma.$disconnect();
     return res.status(201).send(newInstrument);
   }
