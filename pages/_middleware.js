14ae944764cc13f85578fe165698529f9f6ea899
Carla Daniela Rendon <carla.rendon@talosdigital.com>
2022-11-18 16:01:58 -0500

    test 123

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -10,9 +10,6 @@ export async function middleware(req) {
   ) {
     const token = await getToken({ req });
   
-    if (!token) {
-      return NextResponse.redirect(`${origin}/auth/signin`);
-    }
   }
 
   return NextResponse.next();
